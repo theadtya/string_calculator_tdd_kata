32b0f579db50a6911fba1153b58b7b18f60ea29f
Aditya Raj <theadtya@gmail.com>
2025-08-07 20:31:22 +0530

    Add test for multiple numbers

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -17,3 +17,8 @@ def test_add_two_numbers_returns_sum():
     """Test that add method returns sum for two comma-separated numbers"""
     calculator = StringCalculator()
     assert calculator.add("1,5") == 6
+
+def test_add_multiple_numbers_returns_sum():
+    """Test that add method returns sum for multiple comma-separated numbers"""
+    calculator = StringCalculator()
+    assert calculator.add("1,2,3,4,5") == 15
